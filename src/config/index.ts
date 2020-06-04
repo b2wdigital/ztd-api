639efafd0dce008d1b5dca9a9f870e1851f1f19f
Camila Barroso <camila.barroso@b2wdigital.com>
2020-06-04 15:19:50 -0300

    alterando algumas variaveis de ambiente

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const config = {
-  PORT: process.env.PORT || 3333,
+  PORT: process.env.NODE_PORT || 3333,
   MONGO_DB_URL:process.env.MONGO_DB_URL || "",
   CLIENT_ID : process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET
